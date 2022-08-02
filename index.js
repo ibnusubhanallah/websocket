f513e441aa76e5989551d1544fb8e90178ed4122
ibnusubhanallah <hansanakok.aja@gmail.com>
2022-08-02 09:29:55 +0700

    nyoba aja ini mah

---
+++ index.js
@@ -0,0 +1,27 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({port});
+
+/**
+ * @type {WebSocket.WebSocket[]}
+ */
+var admins = [];
+
+wss.on('connection', (ws) => {
+    ws.on('message', (messageAsString) => {
+        /**
+         * @type {{
+         *  pengirim: String, 
+         *  pesanan: Object
+         * }}
+         */
+        const message = JSON.parse(messageAsString);
+        if (message.pengirim.slice(0,12)/* .slice(-8) */ == 'Admin Kantin') {
+            admins.push(ws);
+            // return true; gatau ini bisa nge end 'on' apa engga
+        } else {
+            admins.forEach((a) => {
+                a.send(message.pesanan)
+            })
+        }
+    })
+});
