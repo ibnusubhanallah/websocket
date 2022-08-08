a02fdece9215b0e9a42351fa82cdc17e90eaf942
ibnusubhanallah <hansanakok.aja@gmail.com>
2022-08-08 08:07:22 +0700

    tambah admin

--- index.js
+++ index.js
@@ -5,26 +5,30 @@ const wss = new WebSocket.Server({ port: 8080 });
  * @type {WebSocket.WebSocket[]}
  */
 var admins = []
+
 var pesanans = []
 
 wss.on('connection', (ws) => {
     ws.on('message', (messageAsString) => {
         /**
          * @type {{
-         *  pengirim: String, 
-         *  pesanan: Object
+         *  admin: boolean,
+         *  pembeli: String, 
+         *  pesanan: Object,
+         *  status: Enumerator,
+         *  totalHarga: int
          * }}
          */
         const message = JSON.parse(messageAsString);
         console.log(message)
-        if (message.pengirim.slice(0, 12)/* .slice(-8) */ == 'Admin Kantin') {
+        if (message.admin) {
             admins.push(ws);
             ws.send(JSON.stringify({ tipe: "all order", dataa: pesanans }))
             // return true; gatau ini bisa nge end 'on' apa engga
         } else {
-            pesanans.push(message.pesanan)
+            pesanans.push(message)
             admins.forEach((a) => {
-                a.send(JSON.stringify({ tipe: "notif pesanan baru", dataa: message.pesanan }))
+                a.send(JSON.stringify({ tipe: "notif pesanan baru", dataa: message }))
             })
         }
     })
