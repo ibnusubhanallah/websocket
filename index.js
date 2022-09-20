ab354a642a6ae40493a2f2a27e0b1dc390ef7f55
Farhan <hansanakok.aja@gmail.com>
2022-09-20 13:13:38 +0700

    split two file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
+require('./wa_ekspress');
 
 /**
  * @type {WebSocket.WebSocket[]}
@@ -22,6 +23,7 @@ var wasNotReady = {
 }
 
 function nowReady() {
+    isReady = true;
     wasNotReady.conns.forEach((c, i) => c.send(wasNotReady.msgs[i]))
     wasNotReady.conns = []
     wasNotReady.msgs = []
@@ -80,104 +82,5 @@ wss.on('close', (ws) => {
 })
 
 //----------------------------------------------------------
-
-const express = require('express');
-const path = require('path');
-const app = express();
-const mysql = require('mysql');
-const port = parseInt(process.env.PORT) || 8081;
-
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "kantinasqy"
-})
-//bismillahpasskantinasqysubhanallahmntp
-
-var QRCode = require('qrcode');
-const wwebjs = require('whatsapp-web.js');
-
-const client = new wwebjs.Client({
-    authStrategy: new wwebjs.LocalAuth({ clientId: "farhan" }),
-    puppeteer: { args: ["--no-sandbox", "--disable-setuid-sandbox"] }
-});
-var qrr = "null";
-var sessionn = "null";
-client.on('qr', qr => {
-    QRCode.toDataURL(qr, function (err, url) {
-        qrr = url;
-    });
-});
-
-client.on('disconnected', () => {
-    console.log("disconnected");
-})
-
-app.get('/init/', (req, res) => {
-    client.initialize();
-    res.send("oke")
-})
-
-app.get('/logout/', (req, res) => {
-    client.logout().then(
-        (done) => {
-            res.send("oke");
-        },
-        (fail) => {
-            res.send("gagal");
-        }
-    )
-})
-
-app.get('/update/', (req, res) => {
-    var wstate = "";
-    try {
-        client.getState().then((ws) => {
-            wstate = ws == null ? "null" : ws.toString()
-            sendd()
-        })
-    } catch (error) {
-        console.log(error)
-        sendd()
-    }
-    function sendd() {
-        res.set({ 'Content-Type': 'application/json; charset=utf-8' })
-        res.send(JSON.stringify({
-            qrcode: qrr,
-            wastate: wstate,
-            session: sessionn
-        }));
-    }
-});
-
-app.listen(port, () => {
-    console.log(`App listening on http://localhost:${port}`)
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
-})
-
-client.on('ready', () => {
-    isReady = true;
-
-    //Kantin ASQY
-    client.on('message', (msg) => {
-        console.log(msg.body);
-        const msgs = msg.body.split("\n")
-        if (msgs[0] == "Jangan ubah isi chat ini") {
-            const i = forLogin.randNumbs.findIndex((rn) => rn == msgs[1])
-            if (i != -1) {
-                console.log("kode benar")
-                msg.getContact().then((c) => forLogin.conns[i].send(JSON.stringify({ status: "dapet", nowa: c.number})))
-                msg.reply("Kode yang anda masukkan benar, silahkan kembali ke Aplikasi/Web Kantin ASQY")
-            } else {
-                console.log("kode salah")
-                msg.reply("Maaf kode yang anda masukkan salah atau sedang ada gangguan")
-            }
-        }
-    })
-})
 
 client.initialize();
